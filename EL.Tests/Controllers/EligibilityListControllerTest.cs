4f2ac0eb8e44bbc8553c6f77729751cb1731fad0
Scott Kirkland <srkirkland@gmail.com>
2010-04-26 22:24:27 +0000

    Finally got all of the EL Controller show method tests working -- we now have coverage on getting changed/unchanged ELs, checking the view return types, and checking out JSON get actions methods.  I also had to created the EntityIdSetter in order for us to more easily create duplicate objects. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 3642. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/EL/Trunk/EL;C3994

--- EL.Tests/Controllers/EligibilityListControllerTest.cs
+++ EL.Tests/Controllers/EligibilityListControllerTest.cs
@@ -35,69 +35,6 @@ namespace EL.Tests.Controllers
             CreateELs();
         }
 
-        public void CreateActionTypes()
-        {
-            ActionType at = new ActionType();
-            ActionType at2 = new ActionType();
-
-            at.Name = "My Type";
-            at2.Name = "Another Type";
-
-            using (var ts = new TransactionScope())
-            {
-                ActionTypeBLL.EnsurePersistent(ref at);
-                ActionTypeBLL.EnsurePersistent(ref at2);
-
-                ts.CommittTransaction();
-            }
-        }
-
-        public void CreateELs()
-        {
-            Employee emp = new Employee("999999999")
-            {
-                FirstName = "Scott",
-                LastName = "Kirkland"
-            };
-
-            Department dept = new Department("030000");
-
-            var act = new EL.Core.Domain.Action() { Name = "New Action" };
-
-            Step step = new Step() { Name = "New Step" };
-
-            var title = "7242";
-            
-            Title t = new Title(title) { AbbreviatedName = "Title" };
-            
-
-            Eligibility el = new Eligibility()
-            {
-                CurrentBlankTitle = title,
-                ProposedBlankTitle = title,
-                CurrentTitle = t,
-                TPCCode = title,
-                Comment = "Comment",
-                Action = act,
-                Employee = emp,
-                Department = dept,
-                CurrentStep = step,
-                ProposedStep = step
-            };
-
-            el.OriginalEligibility = el;
-
-            using (var ts = new TransactionScope())
-            {
-                GenericBLL<Employee, string>.EnsurePersistent(ref emp);
-                GenericBLL<Department, string>.EnsurePersistent(ref dept);
-                GenericBLL<EL.Core.Domain.Action, int>.EnsurePersistent(ref act);
-                GenericBLL<Step, int>.EnsurePersistent(ref step);
-                GenericBLL<Title, string>.EnsurePersistent(ref t);
-                EligibilityBLL.EnsurePersistent(ref el);
-            }
-        }
-
         /// <summary>
         ///A test for Show to see that the show methoid completes and correctly populates the changed data variable
         ///</summary>
@@ -117,6 +54,40 @@ namespace EL.Tests.Controllers
             actual = target.Show(changed) as ViewResult;
 
             Assert.AreEqual(changed.Value, actual.ViewData["Changed"]); //Make sure we call the correct view
+        }
+
+        [TestMethod]
+        public void ShowChangedGivesOnlyWithOriginalEl()
+        {
+            EligibilityListController controller = new EligibilityListController();
+            bool? changed = true;
+
+            var result = controller.Show(changed) as ViewResult;
+
+            var elData = result.ViewData.Model as IQueryable<Eligibility>;
+
+            //Now lets make sure that our original els are never null
+            bool anyWithoutOriginalEL = elData.Where(e => e.OriginalEligibility == null).Any();
+
+            Assert.IsFalse(anyWithoutOriginalEL);
+        }
+
+        [TestMethod]
+        public void ShowAllShouldGetAllActiveELs()
+        {
+            EligibilityListController controller = new EligibilityListController();
+            bool? changed = false; //not just the changed ones
+
+            var result = controller.Show(changed) as ViewResult;
+
+            var elData = result.ViewData.Model as IQueryable<Eligibility>;
+
+            //Lets make sure these are always just the active ones
+            bool anyInactive = elData.Where(e => e.IsActive == false).Any();
+
+            Assert.IsFalse(anyInactive);
+            Assert.AreEqual(1, elData.Count()); //Should be one active el
+
         }
 
         [TestMethod]
@@ -149,6 +120,83 @@ namespace EL.Tests.Controllers
 
             Assert.IsNotNull(data);
             Assert.AreEqual(2, data.Count);
+        }
+
+        public void CreateActionTypes()
+        {
+            ActionType at = new ActionType();
+            ActionType at2 = new ActionType();
+
+            at.Name = "My Type";
+            at2.Name = "Another Type";
+
+            using (var ts = new TransactionScope())
+            {
+                ActionTypeBLL.EnsurePersistent(ref at);
+                ActionTypeBLL.EnsurePersistent(ref at2);
+
+                ts.CommittTransaction();
+            }
+        }
+
+        public void CreateELs()
+        {
+            Employee emp = new Employee("999999999")
+            {
+                FirstName = "Scott",
+                LastName = "Kirkland"
+            };
+
+            Department dept = new Department("030000");
+
+            var act = GenericBLL<EL.Core.Domain.Action, int>.GetByName("My Type");
+
+            Step step = new Step() { Name = "New Step" };
+
+            var title = "7242";
+
+            Title t = new Title(title) { AbbreviatedName = "Title" };
+
+
+            Eligibility el = new Eligibility()
+            {
+                CurrentBlankTitle = title,
+                ProposedBlankTitle = title,
+                CurrentTitle = t,
+                TPCCode = title,
+                Comment = "Comment",
+                Action = act,
+                Employee = emp,
+                Department = dept,
+                CurrentStep = step,
+                ProposedStep = step
+            };
+
+            el.OriginalEligibility = el;
+
+            using (var ts = new TransactionScope())
+            {
+                GenericBLL<Employee, string>.EnsurePersistent(ref emp);
+                GenericBLL<Department, string>.EnsurePersistent(ref dept);
+                GenericBLL<EL.Core.Domain.Action, int>.EnsurePersistent(ref act);
+                GenericBLL<Step, int>.EnsurePersistent(ref step);
+                GenericBLL<Title, string>.EnsurePersistent(ref t);
+
+                EligibilityBLL.EnsurePersistent(ref el);
+
+                NHibernateSessionManager.Instance.GetSession().Evict(el);
+                EntityIdSetter.SetIdOf<int>(el, 0);
+
+                EligibilityBLL.EnsurePersistent(ref el);
+
+                NHibernateSessionManager.Instance.GetSession().Evict(el);
+                EntityIdSetter.SetIdOf<int>(el, 0);
+                el.IsActive = true; //Set this one active
+
+                EligibilityBLL.EnsurePersistent(ref el);
+
+                ts.CommittTransaction();
+            }
         }
     }
 }
