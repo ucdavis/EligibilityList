f5167bfa8d91dd418fedaa137f823405908cea39
Scott Kirkland <srkirkland@gmail.com>
2010-04-26 22:25:45 +0000

    Added payrollPerson class/mapping which can pull users from vPayrollPersons which is a view into PPSDataMart. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 6441. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/EL/Trunk/EL;C4042

---
+++ EligibilityList.Core/Domain/PayrollPerson.cs
@@ -0,0 +1,12 @@
+using UCDArch.Core.DomainModel;
+
+namespace EligibilityList.Core.Domain
+{
+    public class PayrollPerson : DomainObjectWithTypedId<string>
+    {
+        public virtual string EmployeeID { get; set; }
+        public virtual string Department { get; set; }
+        public virtual string TitleCode { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
