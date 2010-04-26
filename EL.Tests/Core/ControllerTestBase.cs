9d8f8cc21b3a973eb1d4b1c09437fd488f5bafa3
Scott Kirkland <srkirkland@gmail.com>
2010-04-26 22:24:54 +0000

    Created some common base classes and helpers for testing, and associated rhino mocks and the mvc test helpers. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 4548. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/EL/Trunk/EL;C4015

---
+++ EL.Tests/Core/ControllerTestBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+using EL.Web;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MvcContrib.TestHelper;
+
+namespace EL.Tests.Core
+{
+    [TestClass]
+    public abstract class ControllerTestBase<T> where T : Controller
+    {
+        protected T Controller { get; set; }
+        protected TestControllerBuilder Builder { get; set; }
+
+        protected ControllerTestBase()
+        {
+            new RouteConfigurator().RegisterRoutes();
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            Builder = new TestControllerBuilder();
+            Controller = Builder.CreateController<T>();
+        }
+    }
+}
---
+++ EL.Tests/Core/UserContextHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Principal;
+using EL.Tests.Core.Fakes;
+using Rhino.Mocks;
+
+namespace EL.Tests.Core
+{
+    public static class UserContextHelper
+    {
+        public static IPrincipal StubIPrincipal()
+        {
+            return StubIPrincipal(null, false, false);
+        }
+
+        public static IPrincipal StubIPrincipal(string name, bool isAdmin, bool isUser)
+        {
+            var principal = MockRepository.GenerateStub<IPrincipal>();
+            principal.Expect(p => p.Identity).Return(new FakeIdentity { Name = name });
+            principal.Expect(p => p.IsInRole("Admin")).Return(isAdmin);
+            principal.Expect(p => p.IsInRole("User")).Return(isUser);
+
+            return principal;
+        }
+    }
+}
