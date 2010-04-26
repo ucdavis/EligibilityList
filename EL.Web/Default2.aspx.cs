238a101b26ab25689fa9a353295a6054be91331b
Scott Kirkland <srkirkland@gmail.com>
2010-04-26 22:23:29 +0000

    jQuery, ScriptManagers, basic UI framework (Migrated by TFS Migration Toolkit) (Updated by TFS to TFS Connector, server: http://1x:8080/, id 1585. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/EL/Trunk/EL;C3957

--- EL.Web/Default2.aspx.cs
+++ EL.Web/Default2.aspx.cs
@@ -7,8 +7,7 @@ using System.Web.UI.WebControls;
 using System.Text;
 using EL.BLL;
 
-
-public partial class _Default : System.Web.UI.Page 
+public partial class Default2 : System.Web.UI.Page
 {
     protected void Page_Init(object sender, EventArgs e)
     {
@@ -20,6 +19,6 @@ public partial class _Default : System.Web.UI.Page
         StringBuilder s = new StringBuilder("Hello ");
         s.Append(ActionTypeBLL.GetByID(1).Name);
 
-        Response.Write(s.ToString());
+        Response.Write(s.ToString());  
     }
 }
