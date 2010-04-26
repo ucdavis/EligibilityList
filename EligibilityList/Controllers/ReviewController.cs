7a38e136e71858bf73acd361f51e75ea9bbbd575
Scott Kirkland <srkirkland@gmail.com>
2010-04-26 22:25:38 +0000

    Created the review controller. (Updated by TFS to TFS Connector, server: http://1x:8080/, id 6430. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/EL/Trunk/EL;C4035

---
+++ EligibilityList/Controllers/ReviewController.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Web.Mvc;
+using EligibilityList.Core;
+using EligibilityList.Core.Domain;
+using MvcContrib.Attributes;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Core.Utils;
+using UCDArch.Web.Controller;
+using MvcContrib;
+using UCDArch.Web.Helpers;
+
+namespace EligibilityList.Controllers
+{
+    public class ReviewController : SuperController
+    {
+        /// <summary>
+        /// Review of a pending eligibility request
+        /// </summary>
+        /// <param name="eligibilityId">Id of the temporary eligibility record</param>
+        /// <returns></returns>
+        public ActionResult Index(int eligibilityId)
+        {
+            var viewModel = ElibiilityViewModel.Create(Repository);
+
+            viewModel.Eligibility = Repository.OfType<Eligibility>().GetNullableByID(eligibilityId);
+
+            // not valid redirect to another page
+            if (viewModel.Eligibility == null) return this.RedirectToAction<HomeController>(a => a.Index());
+
+            return View(viewModel);
+        }
+
+        /// <summary>
+        /// POST: /Review/Index/{eligibilityId}
+        /// </summary>
+        /// <param name="eligibilityId">Id of the temporary eligibility record</param>
+        /// <param name="approved"></param>
+        /// <param name="comments"></param>
+        /// <returns></returns>
+        [AcceptPost]
+        public ActionResult Index(int eligibilityId, bool approved, string comments)
+        {
+            var child = Repository.OfType<Eligibility>().GetNullableByID(eligibilityId);
+
+            // not valid redirec to another page
+            if (child == null) return this.RedirectToAction<HomeController>(a => a.Index());
+
+            var parent = child.OriginalEligibility;
+
+            // parent isn't null, something is horribly wrong
+            if (parent == null) return this.RedirectToAction<HomeController>(a => a.Index());
+
+            if (approved)
+            {
+                // copy the changes
+                parent = Copy(child, parent);
+            }
+
+            // validate the parent
+            parent.TransferValidationMessagesTo(ModelState);
+
+            if (ModelState.IsValid)
+            {
+                // save comment
+                parent.Comment = comments;
+
+                // delete the temp
+                Repository.OfType<Eligibility>().Remove(child);
+
+                // save the parent
+                Repository.OfType<Eligibility>().EnsurePersistent(parent);
+
+                // redirect to page
+                return this.RedirectToAction<HomeController>(a => a.Index());
+            }
+
+            // failed, ready the display back out to the user
+            var viewModel = ElibiilityViewModel.Create(Repository);
+            viewModel.Eligibility = child;
+            viewModel.Comment = comments;
+
+            return View(viewModel);
+        }
+
+        private Eligibility Copy(Eligibility source, Eligibility dest)
+        {
+            return dest;
+        }
+    }
+
+    public class ElibiilityViewModel
+    {
+        public Eligibility Eligibility{ get; set; }
+        public string Comment { get; set; }
+
+        public static ElibiilityViewModel Create(IRepository repository)
+        {
+            Check.Require(repository != null, "Repository is required.");
+
+            var viewModel = new ElibiilityViewModel() {};
+
+            return viewModel;
+        }
+    }
+}
