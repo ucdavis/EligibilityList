b965e3c90fc1453347a08c190af2add41d73c589
Scott Kirkland <srkirkland@gmail.com>
2010-04-26 22:26:43 +0000

    Created the el query object/html and view to get an efficient list of ELs (Updated by TFS to TFS Connector, server: http://1x:8080/, id 6751. )
    git-tfs-id: [https://tfs.caes.ucdavis.edu:8088/tfs/DefaultCollection]$/EL/Trunk/EL;C4093

---
+++ EligibilityList.Core/Queries/EligibilityListQuery.cs
@@ -0,0 +1,51 @@
+using System;
+using UCDArch.Core.DomainModel;
+
+namespace EligibilityList.Core.Queries
+{
+    public class EligibilityListQuery : DomainObject
+    {
+        public virtual string Name { get; set; }
+
+        public virtual string ActionType { get; set; }
+
+        public virtual int YearsAtRank { get; set; }
+        public virtual int YearsAtStep { get; set; }
+        public virtual int? YearsAccelerated { get; set; }
+        public virtual int? YearsDecelerated { get; set; }
+
+        public virtual string Committee { get; set; }
+
+        public virtual DateTime? DateDue { get; set; }
+
+        public virtual bool Defer { get; set; }
+        public virtual bool HasOriginalEligibility { get; set; }
+
+        public virtual decimal CurrentAppointmentPercent { get; set; }
+        public virtual string CurrentTitle { get; set; }
+        public virtual string CurrentStep { get; set; }
+
+        public virtual decimal ProposedAppointmentPercent { get; set; }
+        public virtual string ProposedTitle { get; set; }
+        public virtual string ProposedStep { get; set; }
+
+        public virtual string CurrentStatus
+        {
+            get
+            {
+                return string.Format("{0} Step:{1} {2}%", CurrentTitle, CurrentStep, CurrentAppointmentPercent);
+            }
+        }
+
+        public virtual string ProposedStatus
+        {
+            get
+            {
+                if (ProposedTitle == null)
+                    return "No Title";
+                else
+                    return string.Format("{0} Step:{1} {2}%", ProposedTitle, ProposedStep, ProposedAppointmentPercent);
+            }
+        }
+    }
+}
